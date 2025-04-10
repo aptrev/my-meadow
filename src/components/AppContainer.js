3700bcaf40b9f89d70478a2cf74669dd63d06878
Makyra Kim <makyra20412@gmail.com>
2025-04-10 16:27:58 -0500

    minor display scroll fix

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -6,7 +6,7 @@ const AppContainer = ({ theme, children }) => {
         <div className='app position-relative flex-grow-1 m-0 p-0' style={{ backgroundColor: 'var(--primaryLightGreen)' }}>
             <Container
                 fluid
-                className='w-100 m-auto p-4'
+                className='w-100 m-auto p-4 overflow-auto'
                 style={{ height: 'calc(100dvh - 75px)', margin: '64px auto', scrollMarginTop: '75px' }}
             >
                 <Container className='mx-auto d-flex flex-column align-items-center'>
